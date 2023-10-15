f4f38604b9c5d5d3af3631e6449d61776df7e3f5
이하연 <57438406+Hayeon-Lee@users.noreply.github.com>
2023-10-15 16:44:37 +0900

    Update main.py
    /export 라우터를 추가하였습니다.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
 from extractors.remoteok import extract_remoteok_jobs
 from extractors.wwr import extract_wwr_jobs
+from file import save_to_file
 
 app = Flask("JobScrapper")
 
@@ -16,6 +17,8 @@ db = {}
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
     if keyword in db:
         jobs = db[keyword]
     else:
@@ -24,6 +27,17 @@ def search():
         jobs = remoteok + wwr
         db[keyword] = jobs
     return render_template("search.html", keyword=keyword, jobs=db[keyword])
+
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}.csv", as_attachment=True)
 
 
 app.run("0.0.0.0")
